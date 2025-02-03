74ae69c27a961e30784547f3d035285086a419b8
danibreadddev <danpammes@alu.edu.gva.es>
2025-02-03 15:14:47 +0100

    cambios

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* Elementos del DOM*/
 const aboutContainer = document.getElementById("sobreMi");
 const curriculumContainer = document.getElementById("curriculum");
+const title = document.getElementById("title");
 const portfolioContainer = document.getElementById("portfolio");
 const btn_about = document.getElementById("btn_sobreMi");
 const btn_curriculum = document.getElementById("btn_curriculum");
@@ -10,10 +11,23 @@ console.log(aboutContainer);
 /* constantes JS*/
 /* Variabes JS*/
 /* Funciones*/
-function showContainer(showContainer, ocultContainer, ocultContainer2) {
+function showContainer(showContainer, ocultContainer, ocultContainer2, title) {
+  const id = showContainer.getAttribute("id");
   showContainer.style.display = "flex";
   ocultContainer.style.display = "none";
   ocultContainer2.style.display = "none";
+  switch (id) {
+    case "sobreMi":
+      title.innerText = "Sobre Mi";
+      break;
+      case "curriculum":
+      title.innerText = "Curriculum";
+      break;
+  
+    default:
+      title.innerText = "Portfolio";
+      break;
+  }
 }
 function showColor(btn1, btn2, btn3) {
   btn1.style.color = " rgb(195, 223, 17)";
@@ -24,14 +38,14 @@ function showColor(btn1, btn2, btn3) {
 btn_about.style.color = " rgb(195, 223, 17)";
 
 btn_about.addEventListener("click", () => {
-  showContainer(aboutContainer, curriculumContainer, portfolioContainer);
+  showContainer(aboutContainer, curriculumContainer, portfolioContainer,title);
   showColor(btn_about, btn_curriculum, btn_portfolio);
 });
 btn_curriculum.addEventListener("click", () => {
-  showContainer(curriculumContainer, aboutContainer, portfolioContainer);
+  showContainer(curriculumContainer, aboutContainer, portfolioContainer,title);
   showColor(btn_curriculum, btn_portfolio, btn_about);
 });
 btn_portfolio.addEventListener("click", () => {
-  showContainer(portfolioContainer, aboutContainer, curriculumContainer);
+  showContainer(portfolioContainer, aboutContainer, curriculumContainer,title);
   showColor(btn_portfolio, btn_curriculum, btn_about);
 });
